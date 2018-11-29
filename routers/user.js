b44a1d57aacf46ca4211c32725357c635aafc202
kitety <aizaizuori@foxmail.com>
2018-11-30 00:20:41 +0800

    bcrypt安装的注意事项

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const { check, validationResult } = require('express-validator/check');
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+//https://zhuanlan.zhihu.com/p/35156547
 
 let router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }))
@@ -16,6 +18,7 @@ router.post('/register', [
   check('email').isLength({ min: 1 }).withMessage('Email is invalid'),
   check('username').isLength({ min: 1 }).withMessage('Username is invalid'),
   check('password').isLength({ min: 1 }).withMessage('Password is invalid'),
+  // 两次密码验证
   check("password", "invalid password")
     .isLength({ min: 1 })
     .custom((value, { req, loc, path }) => {
@@ -34,15 +37,26 @@ router.post('/register', [
     })
   } else {
     let user = new User(req.body)
-    user.save(err => {
-      if (err) {
-        console.log(err);
-        req.flash("danger", "User Add Failed");
-      } else {
-        req.flash("success", "User Added");
-        res.redirect('/')
-      }
-    })
+    // 需要加密
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        user.password = hash;
+        user.save(err => {
+          if (err) {
+            console.log(err);
+            req.flash("danger", "User Add Failed");
+          } else {
+            req.flash("success", "User Added");
+            res.redirect('/')
+          }
+        })
+      });
+    });
   }
 })
 
